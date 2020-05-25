5da63fd2b1fc1a57c468b9732dd6d31e2f408e77
kiselev21 <kiselev21062000@gmail.com>
2020-05-25 18:48:57 +0300

    копипастил debounce в 3 лабу и случайно изменил lab2

---
+++ lab2/rtl/Debounce.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module Debounce(
+  input         clk_i,
+  input         rst_i,
+  input         en_i, 
+  output        en_down_o
+    );
+
+reg [1:0] sync;
+
+always @( posedge clk_i or negedge rst_i ) begin
+  if ( !rst_i )
+    sync <= 2'b0;
+  else
+    begin
+      sync[0] <= en_i;
+      sync[1] <= sync[0];
+    end
+end
+
+assign en_down_o = ~sync[1] & sync[0];
+    
+  
+endmodule
